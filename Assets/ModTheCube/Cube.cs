658418ed525e6e06647d9786278460f9d36982a9
Sammy Lizon <lizon_sammy@yahoo.co.uk>
2024-10-13 22:44:30 +0100

    Making move and change colour smoothly using LERP.

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -1,23 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+// using System.Drawing;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    public MeshRenderer Renderer;
+    //renderer to manipulate colors and more
+    public Renderer objectRenderer;
+
+    //Get points to move from 
+    public GameObject pointA;
+    public GameObject pointB;
+    private float elapsedTime = 0f;
+    public float moveDuration = 60f;
+
+    private float elapsedTimeC = 0f;
+    public float moveDurationC = 10.0f;
+
+    private Color colorA = Color.red;
+    private Color colorB = Color.blue;
     
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
+        //Increase scale by 1 using shorthand
         transform.localScale = Vector3.one * 1.3f;
         
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+
+        //Call functions to move and change color using LERP functions below
+        SmoothMove();
+        ColorSwitch();
+
+        //Rotate the cube in random direction on all axis
+        transform.Rotate(10.0f * Time.deltaTime, 5.0f * Time.deltaTime, 15.0f * Time.deltaTime);
+
+        //testing moving without LERP - MoveToward()
+        // transform.position = Vector3.MoveTowards(transform.position, pointB.transform.position, 5 * Time.deltaTime);
+
+    }
+
+    void SmoothMove(){
+        //If the time that has passed is less than the time allowed for move then...
+        if(elapsedTime < moveDuration){
+            //..First add to the elapsed time, the time that has passed since last frame 
+            elapsedTime += Time.deltaTime;
+
+            //normalise the time between 0 and 1, then store that for LERP 
+            float t = elapsedTime / moveDuration;
+
+            // Create smooth transition using LERP 
+            transform.position = Vector3.Lerp(pointA.transform.position, pointB.transform.position, t);
+
+        }
+    }
+
+    void ColorSwitch(){
+        if(elapsedTimeC < moveDurationC){
+            //..First add to the elapsed time, the time that has passed since last frame 
+            elapsedTimeC += Time.deltaTime;
+
+            //normalise the time between 0 and 1, then store that for LERP 
+            float f = elapsedTimeC / moveDurationC;
+
+
+            // ...change using smooth transition - LERP 
+            objectRenderer.material.color = Color.Lerp(colorA, colorB, f);
+
+        }
     }
 }
